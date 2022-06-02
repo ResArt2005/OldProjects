f39ceeaab7f99047aec0090f71749921d4f8f655
Artem <resart2005@gmail.com>
2022-06-02 12:13:36 +0400

    First commit

---
+++ Calculator.cpp
@@ -0,0 +1,219 @@
+﻿#include <iostream>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+struct Leksema
+{
+	char type;
+	double value;
+};
+
+bool Maths(stack <Leksema>& Stack_n, stack <Leksema>& Stack_o, Leksema& item)
+{
+	double a, b, c;
+	a = Stack_n.top().value;
+	Stack_n.pop();
+	switch (Stack_o.top().type)
+	{
+	case '+':
+	{
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = a + b;
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+	}
+	break;
+	case '-':
+	{
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = b - a;
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+	}
+	break;
+	case '^':
+	{
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = pow(b, a);
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+	}
+	break;
+	case '*':
+	{
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = a * b;
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+	}
+	break;
+	case '/':
+	{
+		b = Stack_n.top().value;
+		if (a == 0)
+		{
+			cerr << "\nError!\n";
+			return false;
+		}
+		else
+		{
+			Stack_n.pop();
+			c = (b / a);
+			item.type = '0';
+			item.value = c;
+			Stack_n.push(item);
+			Stack_o.pop();
+		}
+	}
+	break;
+	default:
+	{
+		cerr << "\nError!\n";
+		return false;
+	}
+	break;
+	}
+	return true;
+}
+
+int getRang(char ch)
+{
+	if (ch == '^')
+	{
+		return 3;
+	}
+	if (ch == '+' || ch == '-')
+	{
+		return 1;
+	}
+	if (ch == '*' || ch == '/')
+	{
+		return 2;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Enter an expression: ";
+	char ch;
+	double value;
+	bool flag = 1;
+	stack <Leksema> Stack_n; 
+	stack <Leksema> Stack_o;
+	Leksema item;
+	while (1)
+	{
+		ch = cin.peek();
+		if (ch == '\n')
+		{
+			break;
+		}
+		if (ch == ' ')
+		{
+			cin.ignore();
+			continue;
+		}
+		if (ch >= '0' && ch <= '9' || ch == '-' && flag == 1)
+		{
+			cin >> value;
+			item.type = '0';
+			item.value = value;
+			Stack_n.push(item);
+			flag = 0;
+			continue;
+		}
+		if (ch == '+' || ch == '-' && flag == 0 || ch == '*' || ch == '/' || ch == '^')
+		{
+			if (Stack_o.size() == 0)
+			{
+				item.type = ch;
+				item.value = 0;
+				Stack_o.push(item);
+				cin.ignore();
+				continue;
+			}
+			if (Stack_o.size() != 0 && getRang(ch) > getRang(Stack_o.top().type))
+			{
+				item.type = ch;
+				item.value = 0;
+				Stack_o.push(item);
+				cin.ignore();
+				continue;
+			}
+			if (Stack_o.size() != 0 && getRang(ch) <= getRang(Stack_o.top().type))
+			{
+				if (Maths(Stack_n, Stack_o, item) == false)
+				{
+					system("pause");
+					return 0;
+				}
+				continue;
+			}
+		}
+		if (ch == '(')
+		{
+			item.type = ch;
+			item.value = 0;
+			Stack_o.push(item);
+			cin.ignore();
+			continue;
+		}
+		if (ch == ')')
+		{
+			while (Stack_o.top().type != '(')
+			{
+				if (Maths(Stack_n, Stack_o, item) == false)
+				{
+					system("pause");
+					return 0;
+				}
+				else
+				{
+					continue;
+				}
+			}
+			Stack_o.pop();
+			cin.ignore();
+			continue;
+		}
+		else
+		{
+			cout << "\nError!\n";
+			system("pause");
+			return 0;
+		}
+	}
+	while (Stack_o.size() != 0)
+	{
+		if (Maths(Stack_n, Stack_o, item) == false)
+		{
+			system("pause");
+			return 0;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	cout << "Answer: " << Stack_n.top().value << "\n";
+	return 0;
+}
---
+++ Pyatnasshki.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int pyat[4][4] =
+	{ {1,5,3,6},
+	{8,4,2,9},
+	{7,10,14,11},
+	{12,15,13,0} };
+	int a = 1, n;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << pyat[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	cout << "It is pyatnashki: 1 - right, 2 - left, 3 - up, 4 - down, 0 - empty place,\nwhat YOU will be moving" << endl;
+	cout << "999 - exit" << endl;
+	do
+	{
+		cin >> n;
+		switch (n)
+		{
+		case 1:
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					if (pyat[i][j] == 0 && pyat[3][3] != 0 && pyat[2][3] != 0
+						&& pyat[1][3] != 0 && pyat[0][3] != 0)
+					{
+						pyat[i][j] = pyat[i][j + 1];
+						pyat[i][j + 1] = 0;
+					}
+				}
+			}
+			break;
+		}
+		case 2:
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					if (pyat[i][j] == 0 && pyat[3][0] != 0 && pyat[2][0] != 0
+						&& pyat[1][0] != 0 && pyat[0][0] != 0)
+					{
+						pyat[i][j] = pyat[i][j - 1];
+						pyat[i][j - 1] = 0;
+					}
+				}
+			}
+			break;
+		}
+		case 3:
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					if (pyat[i][j] == 0 && pyat[0][3] != 0
+						&& pyat[0][2] != 0 && pyat[0][1] != 0 && pyat[0][0] != 0)
+					{
+						pyat[i][j] = pyat[i - 1][j];
+						pyat[i - 1][j] = 0;
+					}
+				}
+			}
+			break;
+		}
+		case 4:
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					if (pyat[i][j] == 0 && pyat[3][3] != 0
+						&& pyat[3][2] != 0 && pyat[3][1] != 0 && pyat[3][0] != 0)
+					{
+						pyat[i][j] = pyat[i + 1][j];
+						pyat[i + 1][j] = 0;
+					}
+				}
+			}
+			break;
+		}
+		case 999: a = -1; break;
+		default: cout << "That's way doesn't exist!" << endl;
+		}
+		cout << endl;
+
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				cout << pyat[i][j] << "\t";
+			}
+			cout << endl;
+		}
+
+
+	} while (a > 0);
+
+	return 0;
+}
